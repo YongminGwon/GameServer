64a35776c376aad8d8e1ff0118f45920db717f13
Gwon Yong Min <clumsyrhymer@gmail.com>
2025-01-16 14:57:47 +0900

    Add project files

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,16 @@
+#pragma once
+//모든 프로젝트에서 참조할 헤더이므로 많이 쓰이는 헤더들을 넣는다.
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();
